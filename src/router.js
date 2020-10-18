ca518336e4bed0be0cadc785eb43698d93d29f3f
abhinav-TB <abhinavtb@gmail.com>
2020-10-18 19:02:46 +0530

    new changes

---
+++ src/router.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  BrowserRouter,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import App from './App';
+
+
+
+function Router() {
+  return (
+    
+    <BrowserRouter>
+       <Switch>
+          <Route exact path="/"><App/></Route>
+        </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
+
