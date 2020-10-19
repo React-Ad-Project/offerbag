af4cda93772161bb3ad2ebd4bc6e834ebb0d1ced
Ajal333 <ajapoos1999@gmail.com>
2020-10-19 15:20:41 +0530

    changes

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,30 +1,32 @@
-import React,{useState} from 'react';
-import './index.css';
+import React from "react";
+import "./index.css";
 
-
-
-const Card  = (props) => {
-  
-    return(
-        <div className="mainCard">
-            <div className="imageContainer">
-                <img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13.jpg" alt="Sample" />
-            </div>
-            <div className="cardBottom">
-            <div className="Name">
-                    <p>{props.data.name}</p>
-                </div>
-                <div className="price">
-                <del>{props.data.price}</del>
-                <p>{props.data.off_price}</p>
-                </div>
-                <div className="branding">
-                    <img src="https://www.nicepng.com/png/detail/790-7909408_flipkart-accessories-for-mobile-headphones-tablet-icon-of.png" alt="Sample" />
-                </div>
-            </div>
+const Card = (props) => {
+  return (
+    <div className="mainCard">
+      <div className="imageContainer">
+        <img
+          src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13.jpg"
+          alt="Sample"
+        />
+      </div>
+      <div className="cardBottom">
+        <div className="Name">
+          <p>{props.data.name}</p>
         </div>
-
-    )
-}
+        <div className="price">
+          <del>₹{props.data.price}</del>
+          <p>₹{props.data.off_price}</p>
+        </div>
+        <div className="branding">
+          <img
+            src="https://www.nicepng.com/png/detail/790-7909408_flipkart-accessories-for-mobile-headphones-tablet-icon-of.png"
+            alt="Sample"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
--- src/components/offer_details/offer_details.js
+++ src/components/offer_details/offer_details.js
@@ -1,31 +1,37 @@
-import React from 'react'
-import './offer_details.css'
+import React from "react";
+import "./offer_details.css";
 
 export default function Offer_details(props) {
-   
-    return (
-        <div className="details-main">
-            <div className="detail-img">
-                <img className="detail-img" src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15a.jpg"/>
-            </div>
-            <div className="desc-main">
-            <div className="detail-heading">
-                 <h1>Fantasy T-shirt</h1>
-            </div>
-            <div className="detail-price">
-               
-               <del>$12.99</del>
-               <h4>$12.99</h4>
-            </div>
-            <div className="detail-description">
-              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, sapiente illo. Sit error voluptas repellat rerum quidem, soluta enim perferendis voluptates laboriosam. Distinctio, officia quis dolore quos sapiente tempore alias.</p>
-
-            </div>
-            <button type="button" class="btn btn-primary btn-md mr-1 mb-2">Buy now</button>
-            </div>
-        
+  return (
+    <div className="details-main">
+      <div className="detail-img">
+        <img
+          className="detail-img"
+          src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15a.jpg"
+          alt="sample"
+        />
+      </div>
+      <div className="desc-main">
+        <div className="detail-heading">
+          <h1>Fantasy T-shirt</h1>
         </div>
-
-
-    )
+        <div className="detail-price">
+          <del>$12.99</del>
+          <h4>$12.99</h4>
+        </div>
+        <div className="detail-description">
+          <p>
+            {" "}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam,
+            sapiente illo. Sit error voluptas repellat rerum quidem, soluta enim
+            perferendis voluptates laboriosam. Distinctio, officia quis dolore
+            quos sapiente tempore alias.
+          </p>
+        </div>
+        <button type="button" className="btn btn-primary btn-md mr-1 mb-2">
+          Buy now
+        </button>
+      </div>
+    </div>
+  );
 }
